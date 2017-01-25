774a13634e21b494a1046c53b5f66e0784b7edb5
Tyler Crosse <tylerscottcrosse@gmail.com>
2017-01-24 17:52:04 -0800

    Add timestamps to db and messages

--- src/client/components/Chat/Message.jsx
+++ src/client/components/Chat/Message.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import moment from 'moment';
 
-const Message = ({ username, text }) => (
-  <li>
-    {username}: {text}
-  </li>
-);
+const Message = ({ username, text, createdAt }) => {
+  const dateString = moment(createdAt).format('MMM MM HH:mm');
+  return (
+    <li>
+      <p>{username}: {dateString}</p>
+      <p>{text}</p>
+    </li>
+  );
+};
 
 export default Message;
--- src/server/models/Message.js
+++ src/server/models/Message.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 const messageSchema = mongoose.Schema({
   username: String,
   text: String
-});
+}, {timestamps: true});
 
 export default mongoose.model('Message', messageSchema);
