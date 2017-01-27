08fa277325e29457f6d24a2bee3a595052582c16
Tyler Crosse <tylerscottcrosse@gmail.com>
2017-01-27 14:30:36 -0800

    Add server spec

---
+++ src/server/server.spec.js
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { server } from './server.prod.js';
+
+describe('express serving', () => {
+  describe('routing', () => {
+    afterAll(() => {
+      mongoose.connection.close();
+      server.close();
+    });
+
+    it('should respond to / with the index.html', () => {
+      return request(server)
+        .get('/')
+        .expect(200)
+        .expect((res) => expect(res.text).toContain('<div id="root"></div>'));
+    });
+
+    it('should respond to /api/messages with json', () => {
+      return request(server)
+        .get('/api/messages')
+        .expect(200)
+        .expect('Content-Type', /json/);
+    });
+  });
+});
