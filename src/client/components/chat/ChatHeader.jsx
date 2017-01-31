41014a7d5393880b7335a011eff976bca35d36fe
Tyler Crosse <tylerscottcrosse@gmail.com>
2017-01-30 20:45:01 -0800

    Update styles on header

--- src/client/components/chat/ChatHeader.jsx
+++ src/client/components/chat/ChatHeader.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export const ChatHeader = ({ user }) => {
-  const nav = user.loggedIn ? (
-    <img
-      className="avatar"
-      src={'http://i.pravatar.cc/30?u=' + user.username} alt={user.username}
-    />
-  ) : (
-    <Link to="/">Log In</Link>
+  const pathname = window.location.pathname.replace(/\//, '');
+  const userInfo = user.loggedIn && (
+    <div className="chat-user-avatar">
+      <img
+        className="avatar"
+        src={'http://i.pravatar.cc/30?u=' + user.username} alt={user.username}
+      />
+    </div>
   );
   return (
     <header className="chat-header">
-      <Link to="/chat">Chat</Link>
-      {nav}
+      <div className="chat-header-avatar">
+        <img
+          src={'http://i.pravatar.cc/32?u=' + pathname} alt={pathname}
+        />
+      </div>
+      <Link
+        className="chat-header-title"
+        to={'/' + pathname}
+      >
+        {pathname}
+      </Link>
+      {userInfo}
     </header>
   );
 };
