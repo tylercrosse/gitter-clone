1490636dc29a28aca29c1a19ef5c6b9ba47f7476
Tyler Crosse <tylerscottcrosse@gmail.com>
2017-02-02 11:37:06 -0800

    Refactor socketsCtlr and improve testing

--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -1,30 +1,12 @@
 // import fs from 'fs';
 import express from 'express';
-import * as apiCtlr from '../controllers/api';
+import * as messageCtlr from '../controllers/messages';
+import * as staticCtlr from '../controllers/statics';
 
 const router = express.Router();
 
-function renderFullPage(req, res) {
-  // res.sendfile() ?? https://expressjs.com/en/api.html#res.sendFile
-  res.status(200).end(
-`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
-    <title>Gitter Clone</title>
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" rel="stylesheet">
-    <link href="/dist/style.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <div id="root"></div>
-    <script src="/dist/bundle.js"></script>
-  </body>
-</html>`);
-}
-
-router.get('/api/messages', apiCtlr.getMessages);
-router.get('/index.html', renderFullPage);
-router.get('/*', renderFullPage);
+router.get('/api/messages', messageCtlr.getMessages);
+router.get('/index.html', staticCtlr.renderFullPage);
+router.get('/*', staticCtlr.renderFullPage);
 
 export default router;
