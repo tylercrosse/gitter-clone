2f603e731030d8ff08aa70dd7a840348234baed7
Tyler Crosse <tylerscottcrosse@gmail.com>
2017-02-02 21:23:44 -0800

    Add more detailed mock of menu with toggle & animation

--- src/client/components/chat/ChatMenu.jsx
+++ src/client/components/chat/ChatMenu.jsx
@@ -1,14 +1,60 @@
 import React from 'react';
 
-export const ChatMenu = () => (
-  <aside className="chat-menu">
-    <nav>
-      <ul>
-        <li>G</li>
-        <li>Q</li>
-      </ul>
-    </nav>
-  </aside>
-);
+export class ChatMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
+  toggleActive() {
+    this.setState((prevState) => ({
+      active: !prevState.active
+    }));
+  }
+  handleClick() {
+    this.toggleActive();
+  }
+  handleMouseLeave() {
+    this.toggleActive();
+  }
+  render() {
+    const active = this.state.active ? 'active' : '';
+    return (
+      <aside
+        className="chat-menu"
+        onMouseLeave={this.handleMouseLeave}
+      >
+        <section className="chat-menu-container">
+          <nav className="minibar">
+            <div className="minibar-inner">
+              <ul>
+                <li className="minibar-convos">G</li>
+                <li className="minibar-search">
+                  <button onClick={this.handleClick}>
+                    Q
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <section className={'chat-menu-panel ' + active}>
+            <div className="brand-container">Gitter Clone</div>
+            <header className="panel-header">
+              <div className="panel-header-convos">
+                <h2>All Conversations</h2>
+              </div>
+            </header>
+            <div className="panel-inner">
+              {/* links to Conversations */}
+            </div>
+          </section>
+        </section>
+      </aside>
+    );
+  }
+}
 
 export default ChatMenu;
--- src/client/components/chat/ChatMenu.spec.js
+++ src/client/components/chat/ChatMenu.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import ChatMenu from './ChatMenu.jsx';
 
@@ -17,5 +17,37 @@ describe('<ChatMenu />', () => {
     const renderedComponent = renderer.create(component);
     const tree = renderedComponent.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should toggle active state on menu button click', () => {
+    const { component } = setup();
+    const wrapper = mount(component);
+    expect(wrapper.state().active).toBeFalsy();
+    wrapper.find('.chat-menu button').simulate('click');
+    expect(wrapper.state().active).toBeTruthy();
+    wrapper.find('.chat-menu button').simulate('click');
+    expect(wrapper.state().active).toBeFalsy();
+  });
+
+  it('should toggle active state onMouseLeave', () => {
+    const { component } = setup();
+    const wrapper = mount(component);
+    expect(wrapper.state().active).toBeFalsy();
+    wrapper.find('.chat-menu button').simulate('click');
+    expect(wrapper.state().active).toBeTruthy();
+    wrapper.find('.chat-menu').simulate('mouseleave');
+    expect(wrapper.state().active).toBeFalsy();
+  });
+
+  it('should apply class "active" when state is active', () => {
+    const { component } = setup();
+    const wrapper = mount(component);
+    expect(wrapper.state().active).toBeFalsy();
+    expect(wrapper.find('.chat-menu-panel').hasClass('active'))
+      .toBeFalsy();
+    wrapper.find('.chat-menu button').simulate('click');
+    expect(wrapper.state().active).toBeTruthy();
+    expect(wrapper.find('.chat-menu-panel').hasClass('active'))
+      .toBeTruthy();
   });
 });
